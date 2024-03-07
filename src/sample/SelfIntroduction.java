9629c590e789a896f929c9c38ccc86a0ec97b647
KeisukeNakai <5211keisuke@gmail.com>
2024-03-07 10:36:30 +0900

    プロジェクトの作成

---
+++ src/sample/SelfIntroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		//氏名
+		String name ="中井　啓介";
+		//年齢
+		int age = 28;
+		//趣味
+		String hobby ="読書・映画鑑賞、ゲーム、筋トレ、絵を書くこと・見ること";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
