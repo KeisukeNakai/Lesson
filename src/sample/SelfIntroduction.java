566ca47aab72ba091c284bf23d649519a3a5b97b
KeisukeNakai <5211keisuke@gmail.com>
2024-03-08 20:35:43 +0900

    【修正】//　コメントアウトの後ろに半角スペース

--- src/sample/SelfIntroduction.java
+++ src/sample/SelfIntroduction.java
@@ -5,11 +5,11 @@ public class SelfIntroduction {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-		//氏名
+		// 氏名
 		String name ="中井　啓介";
-		//年齢
+		// 年齢
 		int age = 28;
-		//趣味
+		// 趣味
 		String hobby ="読書・映画鑑賞、ゲーム、筋トレ、絵を書くこと・見ること";
 		
 		/*
