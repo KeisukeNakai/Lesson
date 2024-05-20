f5e9fa2d8ba4978fcae465a16970b4ca1f25007f
KeisukeNakai <5211keisuke@gmail.com>
2024-05-20 18:10:15 +0900

    【再提出】5.Java基礎　1.33 VehicleとPersonの紐付け

--- src/process/Bicycle.java
+++ src/process/Bicycle.java
@@ -2,13 +2,5 @@ package process;
 
 public class Bicycle extends Vehicle{
 	
-	private String owner;//所有者
-	
-	//セッターとゲッター
-	public String getOwner() {
-		return owner;
-	}
-	public void setOwner(String owner) {
-		this.owner=owner;
-	}
+
 }
--- src/process/Car.java
+++ src/process/Car.java
@@ -1,14 +1,5 @@
 package process;
 
 public class Car extends Vehicle{
-	
-	private String owner;//所有者
-	
-	//セッターとゲッター
-	public String getOwner() {
-		return owner;
-	}
-	public void setOwner(String owner) {
-		this.owner=owner;
-	}
+		
 }
